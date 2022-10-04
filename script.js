99a3ba6e41ab43a33907e2dbc01f9e51a15508fb
Adams Fontil <adamsfuntil@gmail.com>
2022-10-04 00:01:55 -0400

    Set up cards, and write function to  show items in myLibrary[]

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log('hi')
+let myLibrary = [];
+
+function Books(title, author, pages, read) {
+    this.title = title,
+    this.author = author,
+    this.pages = pages,
+    this.read = read
+
+    // this.sayName = function() {
+    //   console.log(name)
+    // }
+  }
+
+const theHobbit = new Books('The Hobbit', 'J.R.R. Tolkien', '295', 'not read yet')
+const GoT = new Books('The Hobbit', 'J.R.R. Tolkien', '295', 'not read yet')
+const Harry = new Books('The Hobbit', 'J.R.R. Tolkien', '295', 'not read yet')
+
+function addBookToLibrary(book) {
+    myLibrary.push(book)
+}
+addBookToLibrary(theHobbit)
+addBookToLibrary(GoT)
+addBookToLibrary(Harry)
+
+console.log(myLibrary)
+
+function showBooks () {
+    for (books in myLibrary) {
+        console.log(myLibrary[books])
+        target = myLibrary[books]
+        for (item in target) {
+            console.log(target[item])
+        }
+    }
+}
+showBooks()
