850223172444ac17cee6833fda29c0cb708ebfa4
Adams Fontil <adamsfuntil@gmail.com>
2022-10-05 09:06:18 -0400

    Able to remove last book

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let myLibrary = [];
+len = myLibrary.length
 
 const theHobbit = new Books('The Hobbit', 'J.R.R. Tolkien', '295', 'not read yet')
 const GoT = new Books('The Hobbit', 'J.R.R. Tolkien', '295', 'not read yet')
 const Harry = new Books('The Hobbit', 'J.R.R. Tolkien', '295', 'not read yet')
+const removeBtn = document.querySelector('.button')
 
 function addBookToLibrary(book) {
     myLibrary.push(book)
@@ -22,26 +24,42 @@ function showBooks () {
         console.log(myLibrary[len-1])
         target = myLibrary[len-1]
         const div = document.createElement('div')
-        div.className = 'info'
+        div.className = `info${len}`;
         books.append(div)
+        const button = document.createElement('button')
         for (item in target) {
-            console.log(target[item])
             const p = document.createElement('p')
             div.append(p)
             p.innerText = (target[item])
+            div.append(button)
+            button.className = ('button')
+            button.innerText = ('Remove')
         }
-
 }
 
+
+function remove () {
+  console.log(len)
+  len2 = len - 1
+  myLibrary.splice(len, 1)
+  const info = document.querySelector(`.info${len}`)
+  info.remove()
+
+
+}
+removeBtn.addEventListener('click', () => {
+  remove()
+})
+
+
+
+// remove()
 function Books(title, author, pages, read) {
     this.title = title,
     this.author = author,
     this.pages = pages,
     this.read = read
   }
-
-
-
   function addBook (title, author, pages, read ) {
     title = document.querySelector('#title').value
     author = document.querySelector('#author').value;
@@ -50,7 +68,6 @@ function Books(title, author, pages, read) {
     myLibrary.push({title, author, pages, read});
 
   }
-  console.log(myLibrary)
 
 
 const bookBtn = document.querySelector('.bookBtn')
