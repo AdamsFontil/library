e6fc5190c3034a1b923715f7586911838116dfe3
Adams Fontil <adamsfuntil@gmail.com>
2022-12-08 22:55:06 -0500

    Able to make remove button remove book from library

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 let myLibrary = [];
 len = myLibrary.length
 
-
-
 const books = document.querySelector('.books')
 const div = document.createElement('div')
 const p = document.createElement('p')
+
+function Book(title, author, pages, read,) {
+  this.title = title,
+  this.author = author,
+  this.pages = pages,
+  this.read = read,
+  this.number = number
+}
+
+// Book.prototype.sayName = function () {
+//   this.sayName = function() {
+//     console.log(this.name)
+//   }
+// }
 
 function showBooks () {
         len = myLibrary.length
         target = myLibrary[len-1]
         const div = document.createElement('div')
-        div.id = `info ${len}`;
+        div.id = `info`;
         books.append(div)
         const button = document.createElement('button')
         for (item in target) {
@@ -19,48 +31,17 @@ function showBooks () {
             div.append(p)
             p.innerText = (target[item])
             div.append(button)
-            button.id = (`removeBtn`)
+            button.class = (`${len}`)
             button.innerText = ('Remove')
+            button.addEventListener('click',removeFromLibrary)
         }
-}
-
-
-function takeOut () {
-  newNumber = number - 1
-  myLibrary.splice(newNumber, 1)
-  section = `info ${newNumber}`
-  const target2 = document.getElementById(`info ${number}`)
-  target2.remove()
-}
-// I want this to work for every remove button
-const testing = document.querySelector('.removeBtn')
-testing.addEventListener('click', () => {
-  console.log('testing')
-  console.log(div)
-  takeOut()
-  number--
-  // console.log(number)
-  // console.log(myLibrary)
-
-})
-
-// testing how to remove divs
-function removeTest () {
-  console.log('remove test')
-  const removeSection = document.querySelector('.takeOut')
-  removeSection.remove();
+        function removeFromLibrary () {
+          numberToRemove = button.class
+          myLibrary.splice(numberToRemove, 1)
+        }
 
 }
 
-removeTest()
-
-// remove()
-function Book(title, author, pages, read) {
-    this.title = title,
-    this.author = author,
-    this.pages = pages,
-    this.read = read
-  }
 
 
 number = 0
@@ -70,17 +51,21 @@ number = 0
     pages = document.querySelector('#pages').value;
     read = document.querySelector('#read').checked ? document.querySelector('#read').value : 'Not Read';
     number++
-    myLibrary.push({title, author, pages, read, number});
-
+    let newBook = new Book(title, author, pages, read, number);
+    myLibrary.push(newBook);
   }
-  // const removeBtns = document.querySelector(`.info`);
-  // removeBtns.addEventListener('click', () => {
-  //   console.log('remove working')
-  //   console.log(div)
-  //   console.log(len)
-  //   takeOut()
 
-  //     })
+
+
+
+// so that page has books on display
+// for (i =0; i <12; i++) {
+//   addBook()
+//   showBooks()
+//   closeTheForm()
+//   console.log('loopy')
+// }
+
 
 
 const bookBtn = document.querySelector('.bookBtn')
@@ -101,13 +86,3 @@ function openTheForm() {
 function closeTheForm() {
     document.getElementById("form").style.display = "none";
   }
-
-
-
-// so that page has books on display
-for (i =0; i <12; i++) {
-  addBook()
-  showBooks()
-  closeTheForm()
-  console.log('loopy')
-}
--- script2.js
+++ script2.js
@@ -1,28 +1,23 @@
 let myLibrary = [];
 number = 0
 
-function Book(title, author, pages, read) {
-    title = document.querySelector('#title').value
-    author = document.querySelector('#author').value;
-    pages = document.querySelector('#pages').value;
-    read = document.querySelector('#read').checked ? document.querySelector('#read').value : 'Not Read';
+function Book(title, author, pages, read, number) {
+    this.title = title,
+    this.author = author,
+    this.pages = pages,
+    this.read = read
     number++
 }
 
 
 
-const Got = new Book ('Game of Thrones', 'Martin', 230, true)
+
+
+const Got = new Book ('Game of Thrones', 'Martin', 230, true,number++)
 const LotR = new Book ('Lord of the Rings', 'Tolkien', 403, false)
 const Potter = new Book ('Harry Potter', 'Rowling', 140, true)
 
 
 function addBookToLibrary() {
-    myLibrary.push(Got)
-    myLibrary.push(LotR)
-    myLibrary.push(Potter)
-
-
+ myLibrary.push({});
 }
-
-
-addBookToLibrary
